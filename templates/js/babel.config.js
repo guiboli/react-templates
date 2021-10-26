f2f91eb139b983902431a9915027972c82d7fce3
guiboli-dm <guiboli@deepmirror.com>
2021-10-26 10:29:10 +0800

    feat: support async await

--- templates/js/babel.config.js
+++ templates/js/babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
+    [
+      '@babel/transform-runtime',
+      {
+        regenerator: true,
+        corejs: 3,
+      },
+    ],
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-optional-chaining',
   ],
